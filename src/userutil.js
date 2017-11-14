735df63c77dbf4f503d5554349d336477bc5c8b7
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-11-14 10:49:45 -0500

    validate email with code

--- src/userutil.js
+++ src/userutil.js
@@ -1,2 +1,15 @@
+const User = require('./classes/user_.js');
 export class UserUtil {
+//  this.app = app;
+  constructor(){
+    this.userClass = {};
+  //console.log(this.registerClass.register);
+  //this.nevermind = this.registerClass.nevermind;
+  }
+  // activate(){
+  //   this.userClass.verifyEmail();
+  // }
+  attached(){
+    this.userClass = new User();
+  }
 }
