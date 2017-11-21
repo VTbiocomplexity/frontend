8903a8eadd680754f5437d8761c481f2980e6ee6
Joshua Sherman <joshua.v.sherman@gmail.com>
2017-11-21 11:38:17 -0500

    refactoring

--- src/commons/patric.js
+++ src/commons/patric.js
@@ -1,25 +1,30 @@
 exports.showHideElements = function(appName, pArr, nArr){
   let element;
-  if (appName === 'PATRIC'){
-      //loop through pArr and set all to display:block
-    for (let i = 0; i < pArr.length; i++){
-      //console.log(document.getElementsByClassName(pArr[i]));
-      element = document.getElementsByClassName(pArr[i])[0];
-      if (element){
-        element.style.display = 'block';
-      }
-    }
-    //loop though nArr and set all to display:none
-    for (let j = 0; j < nArr.length; j++){
-      document.getElementsByClassName(nArr[j])[0].style.display = 'none';
-    }
-  } else {
-    for (let k = 0; k < pArr.length; k++){
-      document.getElementsByClassName(pArr[k])[0].style.display = 'none';
-    }
-    //loop though nArr and set all to display:none
-    for (let l = 0; l < nArr.length; l++){
-      document.getElementsByClassName(nArr[l])[0].style.display = 'block';
+  for (let i = 0; i < pArr.length; i++){
+    element = document.getElementsByClassName(pArr[i])[0];
+    if (appName === 'PATRIC'){
+      element.style.display = 'block';
+    } else {
+      element.style.display = 'none';
     }
   }
+  for (let j = 0; j < nArr.length; j++){
+    element = document.getElementsByClassName(nArr[j])[0];
+    if (appName === 'PATRIC'){
+      element.style.display = 'none';
+    } else {
+      element.style.display = 'block';
+    }
+    //document.getElementsByClassName(nArr[j])[0].style.display = 'none';
+  }
+// }
+// else {
+//   for (let k = 0; k < pArr.length; k++){
+//     document.getElementsByClassName(pArr[k])[0].style.display = 'none';
+//   }
+//   //loop though nArr and set all to display:none
+//   for (let l = 0; l < nArr.length; l++){
+//     document.getElementsByClassName(nArr[l])[0].style.display = 'block';
+//   }
+// }
 };
