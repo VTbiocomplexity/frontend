686014aa6d04634c6224493ad4e024a829362b5e
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-01-27 09:43:32 -0500

    fixed

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ System.import('isomorphic-fetch');
 System.import('whatwg-fetch');
 //System.import('popper.js');
 import {PLATFORM} from 'aurelia-pal';
-import {inject} from 'aurelia-framework';
+import {inject, bindable} from 'aurelia-framework';
 import {AuthorizeStep, AuthService} from 'aurelia-auth';
 import {UserAccess} from './classes/UserAccess.js';
 import {HttpClient} from 'aurelia-fetch-client';
@@ -12,6 +12,7 @@ export class App {
   constructor(auth, httpClient) {
     this.auth = auth;
     this.httpClient = httpClient;
+    //this.fullmenu = true;
     //this.dashboardTitle = 'Dashboard';
     //this.role = '';
     //this.configHttpClient();
@@ -21,12 +22,122 @@ export class App {
   dashboardTitle = 'Dashboard';
   role = '';
   authenticated = false;
+  email = '';
+  password = '';
+  authenticated = false;
+  token = '';
+  expanded = false;
+
+  @bindable
+  drawerWidth = '175px';
+
+  @bindable
+  fullmenu = true;
+
+  //mobileDrawerOpen = false;
 
   async activate() {
     this.configHttpClient();
     this.appState = new AppState(this.httpClient);
     this.userAccess = new UserAccess(this.appState);
     await this.checkUser();
+  }
+
+  get drawerOpen() {
+    let drawer = document.getElementById('drawerPanel');
+    if (drawer !== null) {
+      if (drawer.selected === 'drawer') {
+        this.hideToggle();
+        return true;
+      }
+    }
+    this.close();
+    return false;
+  }
+
+  get widescreen() {
+    let isWidescreen = document.documentElement.clientWidth > 766;
+    if (isWidescreen) {
+      this.hideToggle();
+    }
+    return isWidescreen;
+  }
+
+  get currentStyles() {
+    let result = {};
+    let style = 'wj';
+    let mobilemenutoggle = document.getElementById('mobilemenutoggle');
+    result = {
+      headerImagePath: '../static/imgs/BI_logo.jpg',
+      headerText1: 'NDSSL',
+      headerClass: 'home-header',
+      headerImageClass: 'home-header-image',
+      sidebarClass: 'home-sidebar',
+      menuToggleClass: 'home-menu-toggle'
+    };
+    result.sidebarImagePath = '../static/imgs/BI_logo2.jpg';
+    if (mobilemenutoggle !== null) {
+      mobilemenutoggle.style.backgroundColor = '#2a222a';
+    }
+    //}
+    this.setFooter(style);
+    return result;
+  }
+
+  setFooter(style) {
+    let footer = document.getElementById('wjfooter');
+    let color = '';
+    if (footer !== null) {
+      footer.style.backgroundColor = '#2a222a';
+      if (style === 'ohaf') {
+        footer.style.backgroundColor = '#565656';
+        color = '#c09580';
+      }
+      footer.innerHTML = '<div style="text-align: center">' +
+      // '<a target="_blank" style="color:' + color + '" href="https://github.com/WebJamApps"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>' +
+      // '<span>&nbsp;&nbsp;</span><a target="_blank" style="color:' + color + '"  href="https://www.linkedin.com/company-beta/16257103"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>' +
+      '<span>&nbsp;&nbsp;</span><a target="_blank" style="color:' + color + '"  href="https://www.facebook.com/biocomplexity/"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>' +
+      // '<span>&nbsp;&nbsp;</span><a target="_blank" style="color:' + color + '"  href="https://plus.google.com/u/1/109586499331294076292"><i class="fa fa-google-plus-square fa-2x" aria-hidden="true"></i></a>' +
+      '<span>&nbsp;&nbsp;</span><a target="_blank" style="color:' + color + '"  href="https://twitter.com/ndssl_bi"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a><br>' +
+      // '<span style="color:white; font-size: 9pt; padding-left:18px;">Powered by ' +
+      // '<a class="wjllc" target="_blank" href="https://www.web-jam.com">Web Jam LLC</a>
+      '</span></div>';
+    }
+  }
+
+  toggleMenu() {
+    //console.debug(this.fullmenu);
+    if (this.fullmenu) {
+      this.fullmenu = false;
+      this.drawerWidth = '50px';
+    } else {
+      this.fullmenu = true;
+      this.drawerWidth = '175px';
+    }
+  }
+
+  close() {
+    let drawer = document.getElementById('drawerPanel');
+    if (drawer !== null) {
+      drawer.closeDrawer();
+      //console.log(drawer);
+    }
+    if (!this.widescreen) {
+      let mobilemenutoggle = document.getElementById('mobilemenutoggle');
+      if (mobilemenutoggle !== null) {
+        mobilemenutoggle.style.display = 'block';
+      }
+    }
+  }
+
+  hideToggle() {
+    //console.log('going to hide you hamburger!');
+    let mobilemenutoggle = document.getElementById('mobilemenutoggle');
+    if (mobilemenutoggle !== null) {
+      mobilemenutoggle.style.display = 'none';
+    }
+    //let drawer = document.getElementById('drawerPanel');
+    //console.log(drawer);
   }
 
   configHttpClient() {
