e366b665129063a215af16877d8f2a86b5255ef6
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-02-07 08:07:26 -0500

    rafter wip

--- src/dashboard-router.js
+++ src/dashboard-router.js
@@ -6,7 +6,8 @@ export class DashboardRouter {
     config.map([
       { route: '', name: 'dashboard', moduleId: PLATFORM.moduleName('./dashboard'), nav: false, title: 'Dashboard', auth: true},
       { route: 'developer', name: 'developer', moduleId: PLATFORM.moduleName('./dashboard-child-routes/developer'), nav: false, title: 'Developer', auth: true},
-      { route: 'user-account', name: 'user-account', moduleId: PLATFORM.moduleName('./dashboard-child-routes/user-account'), nav: true, title: 'User Account', auth: true}
+      { route: 'user-account', name: 'user-account', moduleId: PLATFORM.moduleName('./dashboard-child-routes/user-account'), nav: true, title: 'User Account', auth: true},
+      { route: 'rafter', name: 'rafter', moduleId: PLATFORM.moduleName('./dashboard-child-routes/rafter'), nav: true, title: 'Rafter', auth: true}
     ]);
     this.router = router;
   }
