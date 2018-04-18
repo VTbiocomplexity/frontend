8552a1102e0367648e9373aca2abd4cd7bfad7da
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-04-18 08:23:16 -0400

    refactoring

--- src/dashboard-child-routes/rafter.js
+++ src/dashboard-child-routes/rafter.js
@@ -359,30 +359,30 @@ export class Rafter {
     vsFetch(vsFetchSuccess, myApp, rafterUserID, cmd, myRafterFile, true, mtws, hdjId, hdj, tv, showFile, rvs, displayTree, subDirFiles, mnj, makeFilesClickable, vsFetch);
   }
 
-  rafterFileValidate() {
-    let nub = document.getElementById('uploadButton');
-    nub.style.display = 'none';
-    //console.log('i am validating');
-    //console.log(rafterFilePath.files);
-    if (rafterFilePath.files.length === 0) {
-      alert('no file was selected');
-      return false;
-    }
-    for (let i = 0; i < rafterFilePath.files.length; i++) {
-      let oInput = rafterFilePath.files[i];
-      console.log(oInput.type);
-    // the type is determined automatically during the creation of the Blob.
-    // this value cannot be controlled by developer, hence cannot test it.
-    /* istanbul ignore if*/
-      if (oInput.type === 'text/plain' || oInput.type === 'text/html' || oInput.type === 'application/json') {
-      //console.log('type is a plain text file');
-        nub.style.display = 'block';
-        return true;
-      }
-      alert('Sorry, ' + oInput.type + ' is an invalid file type.');
-      return false;
-    }
-  }
+  // rafterFileValidate() {
+  //   let nub = document.getElementById('uploadButton');
+  //   nub.style.display = 'none';
+  //   //console.log('i am validating');
+  //   //console.log(rafterFilePath.files);
+  //   if (rafterFilePath.files.length === 0) {
+  //     alert('no file was selected');
+  //     return false;
+  //   }
+  //   for (let i = 0; i < rafterFilePath.files.length; i++) {
+  //     let oInput = rafterFilePath.files[i];
+  //     console.log(oInput.type);
+  //   // the type is determined automatically during the creation of the Blob.
+  //   // this value cannot be controlled by developer, hence cannot test it.
+  //   /* istanbul ignore if*/
+  //     if (oInput.type === 'text/plain' || oInput.type === 'text/html' || oInput.type === 'application/json') {
+  //     //console.log('type is a plain text file');
+  //       nub.style.display = 'block';
+  //       return true;
+  //     }
+  //     alert('Sorry, ' + oInput.type + ' is an invalid file type.');
+  //     return false;
+  //   }
+  // }
 
   uploadRafterFile() {
     const httpClient = this.app.httpClient;
